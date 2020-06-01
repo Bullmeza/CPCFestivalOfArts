cceb4a921ece63cf68f24abce09c2caa7e475772
unknown <robert.victor.muresan@gmail.com>
2020-06-01 11:39:09 -0400

    Audio

--- pause.js
+++ pause.js
@@ -1,7 +1,37 @@
+let audio = []
+let spinners;
+
+window.onload = function () {
+    const players = document.getElementsByClassName("plyr");
+    for (var x = 0; x < players.length; x++) {
+        audio.push(new Plyr(players[x]));
+    }
+    spinners = document.getElementsByClassName("audio_player");
+}
+
+
 function pause() {
     let vids = document.getElementsByClassName("vimeo");
     for (var i = 0; i < vids.length; i++) {
         new Vimeo.Player(vids[i]).pause();
+    }
+    for (var i = 0; i < audio.length; i++) {
+        audio[i].pause();
+        audio[i].on("play", play);
+        audio[i].on("pause", pauseSpinner);
+    }
+
+}
+
+function play() {
+    for (var x = 0; x < spinners.length; x++) {
+        spinners[x].classList.add("rotate");
+    }
+}
+
+function pauseSpinner() {
+    for (var x = 0; x < spinners.length; x++) {
+        spinners[x].classList.remove("rotate");
     }
 }
 
